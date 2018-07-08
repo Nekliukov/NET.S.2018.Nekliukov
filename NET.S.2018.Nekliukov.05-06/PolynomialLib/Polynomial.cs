f8a32d9a079de4097aba62da12bf2dbabb62a1af
Nekliukov <roman.nekliukov@gmail.com>
2018-07-09 01:18:30 +0300

    Day 6: There were added +,-,==,!= overloads for operations and chenged ToString() method in the #2

--- NET.S.2018.Nekliukov.05-06/PolynomialLib/Polynomial.cs
+++ NET.S.2018.Nekliukov.05-06/PolynomialLib/Polynomial.cs
@@ -19,7 +19,15 @@ namespace PolynomialLib
         /// <param name="coefficents">The coefficents.</param>
         public Polynomial(double[] coefficents) => this.Coefficents = coefficents;
 
-        #region Properties
+        #region Properties        
+        /// <summary>
+        /// Gets the coefficents.
+        /// </summary>
+        /// <value>
+        /// The coefficents.
+        /// </value>
+        /// <exception cref="System.ArgumentException">There are no coeffiecents to create a polynomial</exception>
+        /// <exception cref="System.ArgumentNullException">Array of coefficents can't be equal to null</exception>
         public double[] Coefficents
         {
             get
@@ -27,7 +35,7 @@ namespace PolynomialLib
                 return coefficents;
             }
 
-            set
+            private set
             {
                 if (value == Array.Empty<double>())
                 {
@@ -42,6 +50,99 @@ namespace PolynomialLib
         public int CoefCount { get => coefficents.Length; private set { } }
         #endregion
 
+        #region Overloaded operations        
+        /// <summary>
+        /// Implements the operator +.
+        /// </summary>
+        /// <param name="firstPoly">The first polynomial.</param>
+        /// <param name="secondPoly">The second polynomial.</param>
+        /// <returns>
+        /// The result of the operator.
+        /// </returns>
+        public static Polynomial operator + (Polynomial firstPoly, Polynomial secondPoly)
+        {
+            Polynomial resultPoly = FindBiggerPolynomial(firstPoly, secondPoly);
+
+            for (int i = 0; i < resultPoly.CoefCount; i++)
+            {
+                try
+                {
+                    resultPoly.coefficents[i] = firstPoly.coefficents[i] + secondPoly.coefficents[i];
+                }
+                catch (IndexOutOfRangeException)
+                {
+                    break;
+                }
+            }
+
+            return resultPoly;
+        }
+
+        /// <summary>
+        /// Implements the operator -.
+        /// </summary>
+        /// <param name="firstPoly">The first polynomial.</param>
+        /// <param name="secondPoly">The second polynomial.</param>
+        /// <returns>
+        /// The result of the operator.
+        /// </returns>
+        public static Polynomial operator - (Polynomial firstPoly, Polynomial secondPoly)
+        {
+            Polynomial resultPoly = FindBiggerPolynomial(firstPoly, secondPoly);
+
+            for (int i = 0; i < resultPoly.coefficents.Length; i++)
+            {
+                try
+                {
+                    resultPoly.coefficents[i] = firstPoly.coefficents[i] - secondPoly.coefficents[i];
+                }
+                catch (IndexOutOfRangeException)
+                {
+                    break;
+                }
+            }
+
+            return resultPoly;
+        }
+
+        /// <summary>
+        /// Implements the operator ==.
+        /// </summary>
+        /// <param name="firstPoly">The first polynomial.</param>
+        /// <param name="secondPoly">The second polynomial.</param>
+        /// <returns>
+        /// The result of the operator.
+        /// </returns>
+        public static bool operator == (Polynomial firstPoly, Polynomial secondPoly)
+        {
+            if (firstPoly.CoefCount != firstPoly.CoefCount)
+            {
+                return false;
+            }
+
+            for (int i = 0; i < firstPoly.CoefCount; i++)
+            {
+                if (Math.Abs(firstPoly.coefficents[i] - secondPoly.coefficents[i]) > EPSILON)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        /// <summary>
+        /// Implements the operator !=.
+        /// </summary>
+        /// <param name="firstPoly">The first polynomial.</param>
+        /// <param name="secondPoly">The second polynomial.</param>
+        /// <returns>
+        /// The result of the operator.
+        /// </returns>
+        public static bool operator !=(Polynomial firstPoly, Polynomial secondPoly)
+            => !(firstPoly == secondPoly);
+        #endregion
+
         #region Overriden methods         
         /// <summary>
         /// Returns <see cref="System.String" /> that represents this instance.
@@ -53,25 +154,24 @@ namespace PolynomialLib
         {
             string resultPoly = string.Empty, sign;
 
-            for (int i = 0; i < CoefCount - 1; i++)
+            for (int i = CoefCount - 1; i > 0; i--)
             {
                 if (coefficents[i] == 0)
                 {
                     continue;
                 }
 
-                sign = (coefficents[i + 1] >= 0) ? " + " : " - ";
+                sign = (coefficents[i - 1] >= 0) ? " + " : " - ";
                 resultPoly = string.Concat(
                     resultPoly,
                     Convert.ToString(Math.Abs(coefficents[i])),
                     "*x^",
-                    Convert.ToString(CoefCount - 1 - i),
+                    Convert.ToString(i),
                     sign);
             }
 
-            resultPoly = (coefficents[0] < 0) ? resultPoly.Insert(0, "-") : resultPoly;
-
-            return string.Concat(resultPoly, Convert.ToString(Math.Abs(Coefficents[CoefCount - 1])));
+            resultPoly = (coefficents[CoefCount - 1] < 0) ? resultPoly.Insert(0, "-") : resultPoly;
+            return string.Concat(resultPoly, Convert.ToString(Math.Abs(Coefficents[0])));
         }
 
         /// <summary>
@@ -124,6 +224,26 @@ namespace PolynomialLib
         }
         #endregion
 
+        #endregion
+
+        #region Private API
+        private static Polynomial FindBiggerPolynomial(Polynomial firstPoly, Polynomial secondPoly)
+        {
+            int firstLen = firstPoly.CoefCount,
+                secondLen = secondPoly.CoefCount;
+            int maxLen = (firstLen >= secondLen) ? firstLen : secondLen;
+            double[] resultCoefs = new double[maxLen];
+            if (firstLen >= secondLen)
+            {
+                Array.Copy(firstPoly.coefficents, resultCoefs, firstLen);
+            }
+            else
+            {
+                Array.Copy(secondPoly.coefficents, resultCoefs, secondLen);
+            }
+
+            return new Polynomial(resultCoefs);
+        }
         #endregion
     }
 }
--- NET.S.2018.Nekliukov.05-06/PolynomialLibTest/PolynomialTests.cs
+++ NET.S.2018.Nekliukov.05-06/PolynomialLibTest/PolynomialTests.cs
@@ -6,10 +6,10 @@ namespace PolynomialLibTest
     [TestFixture]
     public class PolynomialTests
     {
-        [TestCase(new double[] { 4, 1, 63, 0, 5 }, ExpectedResult = "4*x^4 + 1*x^3 + 63*x^2 + 5")]
-        [TestCase(new double[] { 4, 1, -63, 6, 5 }, ExpectedResult = "4*x^4 + 1*x^3 - 63*x^2 + 6*x^1 + 5")]
-        [TestCase(new double[] { -4, -1, -63, 6, -5 }, ExpectedResult = "-4*x^4 - 1*x^3 - 63*x^2 + 6*x^1 - 5")]
-        [TestCase(new double[] { 0, 0, 6}, ExpectedResult = "6")]
+        [TestCase(new double[] { 4, 1, 63, 0, 5 }, ExpectedResult = "5*x^4 + 63*x^2 + 1*x^1 + 4")]
+        [TestCase(new double[] { 4, 1, -63, 6, 5 }, ExpectedResult = "5*x^4 + 6*x^3 - 63*x^2 + 1*x^1 + 4")]
+        [TestCase(new double[] { -4, -1, -63, 6, -5 }, ExpectedResult = "-5*x^4 + 6*x^3 - 63*x^2 - 1*x^1 - 4")]
+        [TestCase(new double[] { 6, 0, 6}, ExpectedResult = "6*x^2 + 6")]
         public static string ToStringTest(double[] coefs)
         {
             Polynomial p = new Polynomial(coefs);
@@ -26,6 +26,45 @@ namespace PolynomialLibTest
             Polynomial p1 = new Polynomial(firstCoefs);
             Polynomial p2 = new Polynomial(secondCoefs);
             return p1.Equals(p2);
+        }
+
+        [TestCase(new double[] { 3, 5, 7 }, new double[] { 1, 2, 3 },
+            ExpectedResult = new double[] { 4, 7, 10 })]
+        [TestCase(new double[] { 3, 5, 7, 8 }, new double[] { 1, 2, 3 },
+            ExpectedResult = new double[] { 4, 7, 10, 8 })]
+        [TestCase(new double[] { -5, 4 }, new double[] { -8, -5, int.MaxValue },
+            ExpectedResult = new double[] { -13, -1, int.MaxValue })]
+        public static double[] SumTest(double[] firstCoefs, double[] secondCoefs)
+        {
+            Polynomial p1 = new Polynomial(firstCoefs);
+            Polynomial p2 = new Polynomial(secondCoefs);
+            Polynomial p3 = p1 + p2;
+            return p3.Coefficents;
+        }
+
+        [TestCase(new double[] { 1.25, 2, 3.55 }, new double[] { 1.25, 2, 3.55 }, ExpectedResult = true)]
+        [TestCase(new double[] { int.MaxValue, 0 }, new double[] { int.MaxValue, 0 }, ExpectedResult = true)]
+        [TestCase(new double[] { 12, 0.000000000004 }, new double[] { 12, 0.0 }, ExpectedResult = false)]
+        [TestCase(new double[] { 2, 3, 4 }, new double[] { 0 }, ExpectedResult = false)]
+        [TestCase(new double[] { 0, -0, 2 }, new double[] { 0, +0, 2 }, ExpectedResult = true)]
+        public static bool EqualOperationTest(double[] firstCoefs, double[] secondCoefs)
+        {
+            Polynomial p1 = new Polynomial(firstCoefs);
+            Polynomial p2 = new Polynomial(secondCoefs);
+            return p1 == p2;
+        }
+
+        [TestCase(new double[] { 1.25, 2, 3.55 }, new double[] { 1.25, 2, 3.56 }, ExpectedResult = true)]
+        [TestCase(new double[] { int.MaxValue, 0 }, new double[] { int.MaxValue, 0 }, ExpectedResult = false)]
+        [TestCase(new double[] { 12, 0.000000000004 }, new double[] { 12, 0.0 }, ExpectedResult = true)]
+        [TestCase(new double[] { 2, 3, 4 }, new double[] { 0 }, ExpectedResult = true)]
+        [TestCase(new double[] { 2, 3, 4 }, new double[] { 2, 3, 4.5 }, ExpectedResult = true)]
+        [TestCase(new double[] { 0, -0, 2 }, new double[] { 0, +0, 2 }, ExpectedResult = false)]
+        public static bool NotEqualOperationTest(double[] firstCoefs, double[] secondCoefs)
+        {
+            Polynomial p1 = new Polynomial(firstCoefs);
+            Polynomial p2 = new Polynomial(secondCoefs);
+            return p1 != p2;
         }
     }
 }
