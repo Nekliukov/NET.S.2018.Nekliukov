f30388e22fd328e77859ed44901b2fd5a1990826
Nekliukov <roman.nekliukov@gmail.com>
2018-07-08 18:17:34 +0300

    Day 6: 50% of the 2nd task have been done: realised only overriden methods

---
+++ NET.S.2018.Nekliukov.05-06/PolynomialLib/Polynomial.cs
@@ -0,0 +1,129 @@
+﻿using System;
+
+namespace PolynomialLib
+{
+    public class Polynomial
+    {
+        #region Constants
+        private const double EPSILON = double.Epsilon;
+        #endregion
+
+        #region Private fields
+        private double[] coefficents;
+        #endregion
+
+        #region Public API        
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Polynomial"/> class.
+        /// </summary>
+        /// <param name="coefficents">The coefficents.</param>
+        public Polynomial(double[] coefficents) => this.Coefficents = coefficents;
+
+        #region Properties
+        public double[] Coefficents
+        {
+            get
+            {
+                return coefficents;
+            }
+
+            set
+            {
+                if (value == Array.Empty<double>())
+                {
+                    throw new ArgumentException("There are no coeffiecents to create a polynomial");
+                }
+
+                coefficents = value ?? throw new
+                    ArgumentNullException("Array of coefficents can't be equal to null");
+            }
+        }
+
+        public int CoefCount { get => coefficents.Length; private set { } }
+        #endregion
+
+        #region Overriden methods         
+        /// <summary>
+        /// Returns <see cref="System.String" /> that represents this instance.
+        /// </summary>
+        /// <returns>
+        /// A <see cref="System.String" /> that represents this instance.
+        /// </returns>
+        public override string ToString()
+        {
+            string resultPoly = string.Empty, sign;
+
+            for (int i = 0; i < CoefCount - 1; i++)
+            {
+                if (coefficents[i] == 0)
+                {
+                    continue;
+                }
+
+                sign = (coefficents[i + 1] >= 0) ? " + " : " - ";
+                resultPoly = string.Concat(
+                    resultPoly,
+                    Convert.ToString(Math.Abs(coefficents[i])),
+                    "*x^",
+                    Convert.ToString(CoefCount - 1 - i),
+                    sign);
+            }
+
+            resultPoly = (coefficents[0] < 0) ? resultPoly.Insert(0, "-") : resultPoly;
+
+            return string.Concat(resultPoly, Convert.ToString(Math.Abs(Coefficents[CoefCount - 1])));
+        }
+
+        /// <summary>
+        /// Determines whether the specified <see cref="System.Object" />, is equal to this instance.
+        /// </summary>
+        /// <param name="obj">The <see cref="System.Object" /> to compare with this instance.</param>
+        /// <returns>
+        ///   <c>true</c> if the specified <see cref="System.Object" /> is equal to this instance;
+        ///   otherwise, <c>false</c>.
+        /// </returns>
+        public override bool Equals(object obj)
+        {
+            if (!(obj is Polynomial))
+            {
+                return false;
+            }
+
+            Polynomial polyToCompare = obj as Polynomial;
+            if (polyToCompare.GetHashCode() == this.GetHashCode())
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// Returns a hash code for this instance.
+        /// </summary>
+        /// <returns>
+        /// A hash code for this instance, suitable for use in hashing algorithms
+        /// and data structures like a hash table. 
+        /// </returns>
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                int hash = 17;
+
+                // get hash code for all items in array
+                foreach (var item in coefficents)
+                {
+                    hash = (hash * 23) + item.GetHashCode();
+                }
+
+                return hash;
+            }
+        }
+        #endregion
+
+        #endregion
+    }
+}
---
+++ NET.S.2018.Nekliukov.05-06/PolynomialLibTest/PolynomialTests.cs
@@ -0,0 +1,31 @@
+﻿using NUnit.Framework;
+using PolynomialLib;
+
+namespace PolynomialLibTest
+{
+    [TestFixture]
+    public class PolynomialTests
+    {
+        [TestCase(new double[] { 4, 1, 63, 0, 5 }, ExpectedResult = "4*x^4 + 1*x^3 + 63*x^2 + 5")]
+        [TestCase(new double[] { 4, 1, -63, 6, 5 }, ExpectedResult = "4*x^4 + 1*x^3 - 63*x^2 + 6*x^1 + 5")]
+        [TestCase(new double[] { -4, -1, -63, 6, -5 }, ExpectedResult = "-4*x^4 - 1*x^3 - 63*x^2 + 6*x^1 - 5")]
+        [TestCase(new double[] { 0, 0, 6}, ExpectedResult = "6")]
+        public static string ToStringTest(double[] coefs)
+        {
+            Polynomial p = new Polynomial(coefs);
+            return p.ToString();
+        }
+
+        [TestCase(new double[] { 4, 1, 63, 0, 5 }, new double[] { 4, 1, 63, 0, 5 }, ExpectedResult = true)]
+        [TestCase(new double[] { 63, 0, 5 }, new double[] { 69, 45 }, ExpectedResult = false)]
+        [TestCase(new double[] { 1, 2, 3 }, new double[] { 3, 2, 1 }, ExpectedResult = false)]
+        [TestCase(new double[] { 1, 2, 3 }, new double[] { 1, 2, -3 }, ExpectedResult = false)]
+        [TestCase(new double[] { 1, 2.001, 3 }, new double[] { 1, 2, 3 }, ExpectedResult = false)]
+        public static bool EqualTest(double[] firstCoefs, double[] secondCoefs)
+        {
+            Polynomial p1 = new Polynomial(firstCoefs);
+            Polynomial p2 = new Polynomial(secondCoefs);
+            return p1.Equals(p2);
+        }
+    }
+}
