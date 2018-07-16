579b1eabca0bc0bc19ce28a5af042ba64db88fb4
Nekliukov <roman.nekliukov@gmail.com>
2018-07-16 09:52:56 +0300

    Dat 9: made a mistake, don't pay attention

--- NET.S.2018.Nekliukov.09/JaggedArrayLibTest/Comparers.cs
+++ NET.S.2018.Nekliukov.09/JaggedArrayLibTest/Comparers.cs
@@ -91,9 +91,7 @@ namespace JaggedArrayLibTest
 
     #region Validator
     /// <summary>
-    /// Class for primitive checks operands on null and refference eqality.
-    /// CASE OF NULL OPERANDS will not happend in our task with integer arrays,
-    /// but it still realised (just maybe we will use it somewhere else in this assembly)
+    /// Class for primitive checks operands on null and refference equality
     /// </summary>
     internal static class CompareValidator
     {
