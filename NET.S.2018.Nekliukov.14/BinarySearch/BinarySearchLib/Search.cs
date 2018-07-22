2aebc0f951992d7bd9c94c4f27e1cb1df881c412
Nekliukov <roman.nekliukov@gmail.com>
2018-07-22 21:06:10 +0300

    Day 14: Binary search algorithm done

---
+++ NET.S.2018.Nekliukov.14/BinarySearch/BinarySearchLib/Search.cs
@@ -0,0 +1,109 @@
+﻿using System;
+
+namespace BinarySearchLib
+{
+    /// <summary>
+    /// Genric class for searching elements
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public static class Search<T>
+    {
+        #region Public API
+        /// <summary>
+        /// Return boolean value, according to existion of an element
+        /// in array, using binary search
+        /// </summary>
+        /// <param name="values">The values.</param>
+        /// <param name="value">The value.</param>
+        /// <param name="compare">The comparer.</param>
+        /// <returns>Boolean value</returns>
+        /// <exception cref="System.ArgumentNullException">Null argument was sent.
+        /// Check input data</exception>
+        /// <exception cref="System.ArgumentException">
+        /// Array is not sorted. Unexpected result
+        /// or
+        /// Empty array was sent
+        /// </exception>
+        public static bool Binary(T[] values, T value, Comparison<T> compare)
+        {
+            if (values == null || value == null)
+            {
+                throw new ArgumentNullException("Null argument was sent. Check input data");
+            }
+
+            if (!IsSorted(values, compare))
+            {
+                throw new ArgumentException("Array is not sorted. Unexpected result");
+            }
+
+            if (values.Length == 0)
+            {
+                throw new ArgumentException("Empty array was sent");
+            }
+
+            if (compare(value, values[0]) < 0 ||
+               (compare(value, values[values.Length - 1]) > 0))
+            {
+                return false;
+            }
+
+            return BinaryAlgorithm(values, value, compare);
+        }
+        #endregion 
+
+        #region Private methods
+        /// <summary>
+        /// The main algorithm for binary searching
+        /// </summary>
+        /// <param name="values">The values.</param>
+        /// <param name="value">The value.</param>
+        /// <param name="compare">The compare.</param>
+        /// <returns>Boolean value</returns>
+        private static bool BinaryAlgorithm(T[] values, T value, Comparison<T> compare)
+        {
+            int first = 0, last = values.Length, middle;
+
+            while (first < last)
+            {
+                middle = first + (last - first) / 2;
+
+                if (compare(value, values[middle]) < 0)
+                {
+                    last = middle;
+                }
+                else if (compare(value, values[middle]) == 0)
+                {
+                    return true;
+                }
+                else
+                {
+                    first = middle + 1;
+                }                    
+            }
+
+            return false;
+        }
+
+        /// <summary>
+        /// Determines whether the specified values is sorted.
+        /// </summary>
+        /// <param name="values">The values.</param>
+        /// <param name="compare">The compare.</param>
+        /// <returns>
+        /// <c>true</c> if the specified values is sorted; otherwise, <c>false</c>.
+        /// </returns>
+        private static bool IsSorted(T[] values, Comparison<T> compare)
+        {
+            for (int i = 0; i < values.Length - 1; i++)
+            {
+                if (compare(values[i],values[i+1]) > 0)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+        #endregion
+    }
+}
---
+++ NET.S.2018.Nekliukov.14/BinarySearch/BinarySearchTest/SearchTest.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace BinarySearchTest
+{
+    [TestFixture]
+    public class SearchTest
+    {
+        [TestCase(new int[] { 1, 2, 3, 4, 5, 6, 7 }, 2, ExpectedResult = true)]
+        [TestCase(new int[] { 1, 2, 3, 4, 5, 6, 7 }, 88, ExpectedResult = false)]
+        [TestCase(new int[] { 1, 2, 3, 4, 5, 6, 7 }, -9, ExpectedResult = false)]
+        [TestCase(new int[] { int.MinValue, -2345, -124, 0, 222, 12445, 12445, int.MaxValue }, 222, ExpectedResult = true)]
+        [TestCase(new int[] { int.MinValue, -2345, -124, 0, 222, 12445, 12445, int.MaxValue }, -2345, ExpectedResult = true)]
+        [TestCase(new int[] { int.MinValue, -2345, -124, 0, 222, 12445, 12445, int.MaxValue }, int.MinValue, ExpectedResult = true)]
+        [TestCase(new int[] { int.MinValue, -2345, -124, 0, 222, 12445, 12445, int.MaxValue }, int.MaxValue, ExpectedResult = true)]
+        [TestCase(new int[] { int.MinValue, -2345, -124, 0, 222, 12445, 12445, int.MaxValue }, 12445, ExpectedResult = true)]
+        public bool TestSearchInt(int[] array, int value)
+            => BinarySearchLib.Search<int>.Binary(array, value, Comparer<int>.Default.Compare);
+
+        [TestCase(new double[] { 1.345, 2.12, 3.999, 4.5, 5.0, 6.1, 7.69 }, 2, ExpectedResult = false)]
+        [TestCase(new double[] { 1.345, 2.12, 3.999, 4.5, 5.0, 6.1, 7.69 }, 2.12, ExpectedResult = true)]
+        public bool TestSearchDouble(double[] array, double value)
+            => BinarySearchLib.Search<double>.Binary(array, value, Comparer<double>.Default.Compare);
+
+        [TestCase(new bool[] { false, false, false, false }, true, ExpectedResult = false)]
+        [TestCase(new bool[] { false, false, false, false, true }, true, ExpectedResult = true)]
+        public bool TestSearchBool(bool[] array, bool value)
+            => BinarySearchLib.Search<bool>.Binary(array, value, Comparer<bool>.Default.Compare);
+
+        [TestCase(new string[] { "Andrew", "Bart", "Bogdan", "Jeffrey", "John", "Roman", "Zane" }, "Roman", ExpectedResult = true)]
+        [TestCase(new string[] { "Andrew", "Bart", "Bogdan", "Jeffrey", "John", "Roman", "Zane" }, "Albahari", ExpectedResult = false)]
+        public bool TestSearchBool(string[] array, string value)
+            => BinarySearchLib.Search<string>.Binary(array, value, Comparer<string>.Default.Compare);
+
+        [TestCase]
+        public void TestSearchCustomClass()
+        {
+            TimerTest.Student[] students = new TimerTest.Student[] {
+                new TimerTest.Student("Bart de Smet"),
+                new TimerTest.Student("Herbert Schildt"),
+                new TimerTest.Student("John Skeet"),              
+                new TimerTest.Student("Roman Nekliukov")
+            };
+
+            TimerTest.Student value = new TimerTest.Student("Roman Nekliukov");
+            int StudentsComparer(TimerTest.Student lhs, TimerTest.Student rhs)
+            {
+                return Comparer<string>.Default.Compare(lhs.Name, rhs.Name);
+            }
+
+            Assert.IsTrue(BinarySearchLib.Search<TimerTest.Student>.Binary(students, value, StudentsComparer) == true);
+        }
+
+        [TestCase(new int[] { 1, 2, 3, 200, 5, 6, 7 }, 2)]
+        [TestCase(new int[] {}, 2)]
+        public void TestSearchIntArgException(int[] array, int value)
+            => Assert.Throws<ArgumentException>(()=>
+            BinarySearchLib.Search<int>.Binary(array, value, Comparer<int>.Default.Compare));
+
+        [TestCase(new string[] { "Andrew", "Bart", "Bogdan", "Jeffrey", "John", "Roman", "Zane" }, null)]
+        [TestCase(null, "Albahari")]
+        public void TestSearchStringArgNullExcpetion(string[] array, string value)
+            => Assert.Throws<ArgumentNullException>(() => 
+            BinarySearchLib.Search<string>.Binary(array, value, Comparer<string>.Default.Compare));
+    }     
+}
