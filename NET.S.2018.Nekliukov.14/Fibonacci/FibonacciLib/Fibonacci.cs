9ca92d4636dfd2907eb28c1a3c1f54165a71e8d7
Nekliukov <roman.nekliukov@gmail.com>
2018-07-22 17:19:46 +0300

    Day 14: Fibonacci task done

---
+++ NET.S.2018.Nekliukov.14/Fibonacci/FibonacciLib/Fibonacci.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FibonacciLib
+{
+    /// <summary>
+    /// Class for operation with Fibonacci numbers
+    /// </summary>
+    public static class Fibonacci
+    {
+        /// <summary>
+        /// Generates the numbers.
+        /// </summary>
+        /// <param name="numOfNumbers">The number of numbers.</param>
+        /// <returns></returns>
+        /// <exception cref="System.ArgumentException">
+        /// Number of elements must be more than 0
+        /// or
+        /// Number value is too long."
+        /// </exception>
+        public static long[] GenerateNumbers(int numOfNumbers)
+        {
+            if (numOfNumbers <= 0)
+            {
+                throw new ArgumentException("Number of elements must be more than 0");
+            }
+
+            List<long> numbers = new List<long>(numOfNumbers);
+            int firstNum = 0, secondNum = 1, nextNum;
+            numbers.Add(firstNum); numbers.Add(secondNum);
+            while (numbers.Count < numOfNumbers)
+            {
+                try
+                {
+                    nextNum = checked(firstNum + secondNum);
+                }
+                catch (OverflowException)
+                {
+                    throw new ArgumentException("Number value is too long. Max limit is " +
+                        $"{numbers.Count - 1}");
+                }
+                numbers.Add(nextNum);
+                firstNum = secondNum;
+                secondNum = nextNum;
+            }
+
+            return (numOfNumbers < 2)?numbers.GetRange(0,1).ToArray():numbers.ToArray();
+        }
+    }
+}
---
+++ NET.S.2018.Nekliukov.14/Fibonacci/FibonacciTest/FibonacciTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using NUnit.Framework;
+using FibonacciLib;
+
+namespace FibonacciLibTest
+{
+    [TestFixture]
+    public class FibonacciTest
+    {
+        [TestCase(1, ExpectedResult = new long[] { 0 })]
+        [TestCase(2, ExpectedResult = new long[] { 0, 1 })]
+        [TestCase(5, ExpectedResult = new long[] { 0, 1, 1, 2, 3 })]
+        [TestCase(23, ExpectedResult = new long[] { 0, 1, 1, 2, 3, 5, 8, 13, 21,
+            34, 55, 89, 144, 233, 377, 610, 987, 1597, 2584, 4181, 6765, 10946, 17711})]
+        [TestCase(47, ExpectedResult = new long[] { 0,1,1,2,3,5,8,13,21,34,55,89,144,
+            233,377,610,987,1597,2584,4181,6765,10946,17711,28657,46368,75025,121393,196418,317811,
+            514229,832040,1346269,2178309,3524578,5702887,9227465,14930352,24157817,39088169,
+            63245986,102334155,165580141,267914296,433494437,701408733,1134903170, 1836311903 })]
+        public long[] NumbersGeneratingTest(int num) => Fibonacci.GenerateNumbers(num);
+
+        [TestCase(-2)]
+        [TestCase(0)]
+        [TestCase(48)]
+        public void NumbersGeneratingArgumentException(int num)
+            => Assert.Throws<ArgumentException>(() => Fibonacci.GenerateNumbers(num));
+    }
+
+
+}
